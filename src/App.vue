c9132fe84c750a7e96dd2d1a52c0b79ea2df611e
amirb <amir.sbrito@gmail.com>
2022-04-27 00:47:49 -0300

    feat: added form component + bug fix button icons

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,20 @@
     <div class="column is-one-quarter">
       <SideBar />
     </div>
-    <div class="column is-three-quarter"></div>
+    <div class="column is-three-quarter">
+      <FormTracker />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import SideBar from "./components/SideBar.vue";
+import FormTracker from "./components/FormTracker.vue";
 
 export default defineComponent({
   name: "App",
-  components: { SideBar },
+  components: { SideBar, FormTracker },
 });
 </script>
 
