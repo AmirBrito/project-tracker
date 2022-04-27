c9884f6335cc2aec5e127e34787b02752cda722d
amirb <amir.sbrito@gmail.com>
2022-04-27 00:00:22 -0300

    feat: added sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="column is-one-quarter">
-
+      <SideBar />
     </div>
-    <div class="column is-three-quarter">
-
-    </div>
+    <div class="column is-three-quarter"></div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SideBar from "./components/SideBar.vue";
 
 export default defineComponent({
   name: "App",
+  components: { SideBar },
 });
 </script>
 
