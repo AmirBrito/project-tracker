29f026dbca0482755d4d189ab68e7bc07f2570e2
amirb <amir.sbrito@gmail.com>
2022-06-22 23:35:27 -0300

    feat: adiciona novo component task

---
+++ src/components/TaskTracker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">Task Description</div>
+      <div class="column">
+        <StopWatch :time-in-seconds="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import StopWatch from "./StopWatch.vue";
+
+export default defineComponent({
+  name: "TaskTracker",
+  components: {
+    StopWatch,
+  },
+});
+</script>
+
+<style scoped>
+.box {
+  background: #faf0ca;
+}
+</style>
