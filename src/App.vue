517e3950fd05c9c1bf7bb71bc34d0314448f48ff
amirb <amir.sbrito@gmail.com>
2022-06-23 14:47:42 -0300

    feat: implementa o component box wrapper e renderiza condicional a task default

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
           :key="index"
           :task="task"
         />
+        <BoxTracker v-if="isEmptyList"> Start a task today! </BoxTracker>
       </div>
     </div>
   </main>
@@ -22,14 +23,20 @@ import SideBar from "./components/SideBar.vue";
 import FormTracker from "./components/FormTracker.vue";
 import TaskTracker from "./components/TaskTracker.vue";
 import ITaskTracker from "./interfaces/ITaskTracker";
+import BoxTracker from "./components/BoxTracker.vue";
 
 export default defineComponent({
   name: "App",
-  components: { SideBar, FormTracker, TaskTracker },
+  components: { SideBar, FormTracker, TaskTracker, BoxTracker },
   data() {
     return {
       taskList: [] as ITaskTracker[],
     };
+  },
+  computed: {
+    isEmptyList(): boolean {
+      return this.taskList.length === 0;
+    },
   },
   methods: {
     saveTask(task: ITaskTracker) {
